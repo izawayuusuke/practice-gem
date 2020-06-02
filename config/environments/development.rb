d2b878d82bf80ad540a78bbc0603d1dc377d224f
izawayuusuke <yuu0502@gmail.com>
2020-06-02 06:40:10 +0000

    add mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,16 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.action_mailer.default_url_options = { hos: 'localhost', port: 3000 }
   config.cache_classes = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV['ADDRESS_NAME'],
+    password: ENV['ADDRESS_PASS'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # Do not eager load code on boot.
   config.eager_load = false
 
