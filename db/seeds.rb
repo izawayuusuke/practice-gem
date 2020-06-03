e132bd4075759964043aad96ba072e366ee0a77b
izawayuusuke <yuu0502@gmail.com>
2020-06-03 02:03:24 +0000

    add active_admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,4 @@
               password: "password",
               password_confirmation: "password")
 end
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
